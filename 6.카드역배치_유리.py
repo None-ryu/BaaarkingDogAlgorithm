f0ed35f7e80bf9dd2b6716aa00a6fbee2204e75a
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-03-03 04:34:49 +0900

    구현

---
+++ 6.카드역배치_유리.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/10804
+
+arr = [i for i in range(20+1)]
+for i in range(10):
+    a, b = map(int, input().split())
+
+    tmparr = []
+    # [10,9,8,7,6,5] 뒤집는다.
+    for tmp in range(b, a-1, -1):
+        tmparr.append(arr[tmp])
+    
+    check =0  
+    for j in range(a, b+1):
+        arr[j] = tmparr[check]
+        check += 1
+    print(*arr)
+arr.pop(0)
+print(*arr)
