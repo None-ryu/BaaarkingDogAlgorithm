30d7b3daf07df00712bf136150dff082cc088b71
None-ryu <koujiro10235@naver.com>
2022-03-03 02:33:56 +0900

    5. 구현_현지

--- 5.구현_현지.py
+++ 5.구현_현지.py
@@ -1,7 +1,14 @@
+# N, X = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = []
+# for i in range(N):
+#     if A[i] < X:
+#         B.append(A[i])
+# print(*B)
+
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
-B = []
-for i in range(N):
-    if A[i] < X:
-        B.append(A[i])
-print(*B)
+for i in A:
+    if i > X:
+        A.pop(i) # pop은 번호로 호출해서 불가능
+print(*A)
