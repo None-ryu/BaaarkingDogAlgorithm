4631da545ae877ce330169a816963a77d505740f
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-03-09 04:14:25 +0900

    2강 배열테스트 예제 구현

---
+++ 25.array_test_유리.py
@@ -0,0 +1,33 @@
+# 1 (6) [2] 3  [4] 5
+def insert(idx, num, arr):
+    arr.append(num)
+    for i in range(len(arr)-2, idx-1, -1):
+        arr[i+1] = arr[i] 
+    arr[idx] = num
+
+def erase(idx, arr):
+    for i in range(idx, len(arr)-1):
+        arr[i]=arr[i+1]
+    arr.pop()
+
+# 0 1 () 2 3 4
+def insertTest():
+    arr = [10,20,30]
+    insert(3, 40, arr)
+    print(arr)
+    insert(1, 50, arr)
+    print(arr)
+    insert(0, 15, arr)
+    print(arr)
+
+def eraseTest():
+    arr = [10, 50, 40, 30, 70, 20]
+    erase(4, arr)
+    print(arr)
+    erase(1, arr)
+    print(arr)
+    erase(3, arr)
+    print(arr)
+    
+insertTest()
+eraseTest()
