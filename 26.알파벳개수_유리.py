86b72f0822e8b7a25328a0e62dd622584fe040f5
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-03-09 06:09:25 +0900

    연습문제1

--- 26.알파벳개수_유리.py
+++ 26.알파벳개수_유리.py
@@ -1,15 +1,21 @@
 # https://www.acmicpc.net/problem/10808
-word = input()
-result = []
-for num in range(97, 123):
-    alpha = chr(num)
-    cnt = 0
-    for w in word:
-        if alpha == w:
-            cnt+=1
-    result.append(cnt)
-print(*result)
+# word = input()
+# result = []
+# for num in range(97, 123):
+#     alpha = chr(num)
+#     cnt = 0
+#     for w in word:
+#         if alpha == w:
+#             cnt+=1
+#     result.append(cnt)
+# print(*result)
 
 
 # 97 a
 # 122  z
+
+word = input()
+result = [0]*26
+for w in word:
+    result[ord(w)-97] += 1
+print(*result)
