e76d74075a8768ba70580524105cc6c556c9ec51
None-ryu <koujiro10235@naver.com>
2022-03-10 02:53:09 +0900

    26번과 내용 교환

--- 27.알파벳개수_유리.py
+++ 27.알파벳개수_유리.py
@@ -1,21 +1,12 @@
-# https://www.acmicpc.net/problem/10808
-# word = input()
-# result = []
-# for num in range(97, 123):
-#     alpha = chr(num)
-#     cnt = 0
-#     for w in word:
-#         if alpha == w:
-#             cnt+=1
-#     result.append(cnt)
-# print(*result)
+n = 1
+for i in range(3):
+    N = int(input())
+    n *= N
+n = str(n)
 
-
-# 97 a
-# 122  z
-
-word = input()
-result = [0]*26
-for w in word:
-    result[ord(w)-97] += 1
-print(*result)
+result = [0]*10
+for i in n: # 14111  0 4 0 0 1 .....
+    result[int(i)]+=1
+    
+for i in result:
+    print(i)
