9a1cb6f2da6076dbd428ae660cc07087a30e3239
None-ryu <koujiro10235@naver.com>
2022-03-10 02:00:02 +0900

    내용 추가

--- 2.시간복잡도_현지.py
+++ 2.시간복잡도_현지.py
@@ -1,13 +1,28 @@
-def func2(arr, N):
-    cnt = 0
+# def func2(arr, N):
+#     cnt = 0
+#     for i in range(N):
+#         for j in range(N):
+#             if i != j and arr[i]+arr[j] == 100:
+#                 cnt = 1
+#     if cnt == 1:
+#         print(1)
+#     else:
+#         print(0)
+# func2([1, 52, 48], 3)
+# func2([50, 42], 2)
+# func2([4, 13, 63, 87], 4)
+
+def fun2(arr, N): #4 13 63 87
+    cnt = [0]*100
+    check = 0
     for i in range(N):
-        for j in range(N):
-            if i != j and arr[i]+arr[j] == 100:
-                cnt = 1
-    if cnt == 1:
-        print(1)
-    else:
+        if cnt[100-arr[i]] == 1: #100-87 cnt[13]
+            check = 1
+            print(1)
+            break
+        cnt[arr[i]] = 1 #cnt[4][13][63] = 1
+    if check == 0:
         print(0)
-func2([1, 52, 48], 3)
-func2([50, 42], 2)
-func2([4, 13, 63, 87], 4)
+fun2([1, 52, 48], 3)
+fun2([50, 42], 2)
+fun2([4, 13, 63, 87], 4)
