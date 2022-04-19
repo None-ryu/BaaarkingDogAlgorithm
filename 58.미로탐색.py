083d8213c115f7fb345705cf54523f464234407c
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-04-20 01:18:07 +0900

    bfs

---
+++ 58.미로탐색.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/2178
+from collections import deque
+N, M = map(int, input().split())
+graph = []
+for i in range(N):
+    tlist = list(map(int, input()))
+    graph.append(tlist)
+
+visited = [ [False]*M for _ in range(N)]
+
+def bfs(x, y):
+    dx = [0, 0 , 1, -1]
+    dy = [1, -1, 0, 0]    
+    dq = deque([])
+    dq.append([x, y, 1])
+    visited[y][x] = True
+    while dq:
+        xx, yy, cnt = dq.popleft()        
+        for i in range(4):
+            tmpx = xx+dx[i]
+            tmpy = yy+dy[i]
+            if 0<=tmpx<M and 0<=tmpy<N and visited[tmpy][tmpx] is False and graph[tmpy][tmpx] == 1:
+                visited[tmpy][tmpx] = True                
+                dq.append([tmpx, tmpy, cnt+1])
+            
+            if tmpx == M-1 and tmpy == N-1:
+                print(cnt+1)
+                return
+bfs(0, 0)
