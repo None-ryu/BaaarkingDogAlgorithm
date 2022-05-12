a2ae2e54cd57049a21bbe338a14eb7302563ca65
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-05-13 00:21:53 +0900

    BFS

---
+++ 76.숨바꼭질3.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+N, K = map(int, input().split())
+visited = [False] * 100001
+
+dq = deque([])
+dq.append([N, 0])
+visited[N] = True
+pos = [2, 1, -1]
+while dq:
+    num, cnt = dq.popleft()
+    if num == K:
+        print(cnt)
+        break
+    for i in range(3):
+        tmp = num+pos[i]
+        if pos[i] == 2:
+            tmp = num*2
+
+        if 0<=tmp and tmp<=100000 and visited[tmp] is False:
+            if pos[i] == 2:            
+                dq.appendleft([tmp, cnt])
+            else:            
+                dq.append([tmp, cnt+1])
+            visited[tmp] = True
+        
