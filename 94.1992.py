9763346cb6437583375a8630695e268d6a6be14f
None-ryu <koujiro10235@naver.com>
2022-06-09 02:48:58 +0900

    94. 1992

---
+++ 94.1992.py
@@ -0,0 +1,36 @@
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input())))
+result = ""
+def section(stx, sty, K):
+    tmpx = -1
+    tmpy = -1
+    for i in range(sty, sty+K):
+        if i == 0 or i%(K//2) == 0:
+            tmpy = i
+            for j in range(stx, stx+K):
+                if j == 0 or j%(K//2) == 0:
+                    tmpx = j
+                    color(tmpx, tmpx+K//2, tmpy, tmpy+K//2, K//2)
+def color(stx, edx, sty, edy, K):
+    global result
+    if K == 1:
+        if graph[sty][stx] == 0:
+            result += "0"
+        elif graph[sty][stx] == 1:
+            result += "1"
+        return
+    flag = False
+    for i in range(sty, edy):
+        for j in range(stx, edx):
+            if graph[i][j] != graph[sty][stx]:
+                flag = True
+                result += "("
+                section(stx, sty, K)
+                result += ")"
+                return
+    if flag == False:
+        result += str(graph[sty][stx])
+color(0, N, 0, N, N)
+print(result)
