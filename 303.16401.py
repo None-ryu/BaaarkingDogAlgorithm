8209c94f9971a6912c0df3685d7d04eedb01c287
None-ryu <koujiro10235@naver.com>
2022-08-28 12:33:52 +0900

    303. 16401 내용수정

--- 303.16401.py
+++ 303.16401.py
@@ -1,18 +1,44 @@
+# # pypy로만 통과
+# import sys
+# input = sys.stdin.readline
+# M, N = map(int, input().split())
+# snack = sorted(list(map(int, input().split())))
+# st = 1
+# ed = snack[-1]
+# answer = 0
+# while st <= ed:
+#     mid = (st+ed)//2
+#     cnt = 0
+#     for i in snack:
+#         cnt += i//mid
+#     if cnt >= M:
+#         st = mid+1
+#         answer = max(mid, answer)
+#     else:
+#         ed = mid-1
+# print(answer)
+
+# python3로도 통과
 import sys
 input = sys.stdin.readline
 M, N = map(int, input().split())
-snack = sorted(list(map(int, input().split())))
+snack = list(map(int, input().split()))
+snack.sort()
 st = 1
 ed = snack[-1]
-answer = 0
+
+def line(mid): 
+    #print(mid)   
+    cnt = 0
+    for num in snack:
+        cnt += (num // mid)
+    return cnt
+
 while st <= ed:
     mid = (st+ed)//2
-    cnt = 0
-    for i in snack:
-        cnt += i//mid
+    cnt = line(mid)
     if cnt >= M:
         st = mid+1
-        answer = max(mid, answer)
     else:
         ed = mid-1
-print(answer)
+print(ed)
