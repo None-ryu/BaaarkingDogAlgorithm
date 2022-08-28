107ef8f25766254b24e6c823f061e168e18baded
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-08-28 11:23:07 +0900

    b

--- 302.차집합.py
+++ 302.차집합.py
@@ -7,21 +7,19 @@ narr = list(map(int, input().split()))
 narr.sort()
 marr = list(map(int, input().split()))
 marr.sort()
-
-totalarr = list(set(narr+marr))
-totalarr.sort()
 #print(totalarr)
 
-def binary_search(arr, target):        
+def binary_search(target):        
+    global marr
     left = 0
-    right = len(arr)-1
+    right = len(marr)-1
 
     while left <= right:
         mid = (left + right) // 2
 
-        if arr[mid] == target:
+        if marr[mid] == target:
             return True
-        if arr[mid] < target:
+        if marr[mid] < target:
             left = mid + 1
         else:
             right = mid - 1
@@ -29,14 +27,12 @@ def binary_search(arr, target):
     return False      
 
 ans = []
-for i in totalarr:
-    if binary_search(narr, i) is True:
-        if binary_search(marr, i) is False:
-            ans.append(i)
+for i in narr:
+    if binary_search(i) is False:
+        ans.append(i)
 
 print(len(ans))
-if len(ans) > 0:
-    print(*ans)
+print(*ans)
 
 
 """
