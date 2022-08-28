21133fff5c2274b80aeee5815da26247327f0a1c
None-ryu <koujiro10235@naver.com>
2022-08-28 10:26:45 +0900

    300. 1654 내용수정

--- 300.1654.py
+++ 300.1654.py
@@ -1,3 +1,25 @@
+# import sys
+# input = sys.stdin.readline
+# K, N = map(int, input().split())
+# line = []
+# for _ in range(K):
+#     line.append(int(input()))
+# line.sort()
+# st = 1
+# ed = line[-1]
+# answer = 1
+# while st <= ed:
+#     mid = (st+ed)//2
+#     cnt = 0
+#     for i in line:
+#         cnt += i//mid
+#     if cnt >= N:
+#         st = mid+1
+#         answer = mid
+#     else:
+#         ed = mid-1
+# print(answer)
+
 import sys
 input = sys.stdin.readline
 K, N = map(int, input().split())
@@ -7,7 +29,6 @@ for _ in range(K):
 line.sort()
 st = 1
 ed = line[-1]
-answer = 1
 while st <= ed:
     mid = (st+ed)//2
     cnt = 0
@@ -15,7 +36,6 @@ while st <= ed:
         cnt += i//mid
     if cnt >= N:
         st = mid+1
-        answer = mid
     else:
         ed = mid-1
-print(answer)
+print(ed)
